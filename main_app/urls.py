0582429637d762dbe1e3fc3c3f36acdf51e06c0d
daroo22 <rodneysomto@gmail.com>
2024-09-12 10:57:12 -0400

    progress

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import Home, CatList, CatDetail
 # additional imports
-from .views import Home, CatList, CatDetail, FeedingListCreate, FeedingDetail, ToyList, ToyDetail 
+from .views import Home, CatList, CatDetail, FeedingListCreate, FeedingDetail, ToyList, ToyDetail, AddToyToCat, RemoveToyFromCat
 
 
 urlpatterns = [
@@ -13,4 +13,7 @@ urlpatterns = [
 	path('cats/<int:cat_id>/feedings/<int:id>/', FeedingDetail.as_view(), name='feeding-detail'),
   path('toys/', ToyList.as_view(), name='toy-list'),
   path('toys/<int:id>/', ToyDetail.as_view(), name='toy-detail'),
+  path('cats/<int:cat_id>/add_toy/<int:toy_id>/', AddToyToCat.as_view(), name='add-toy-to-cat'),
+  path('cats/<int:cat_id>/remove_toy/<int:toy_id>/', RemoveToyFromCat.as_view(), name='remove-toy-from-cat'),
+
 ]
